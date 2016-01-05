c5d5aae52c947141359d3722067c708899054b90
Agustin Camino <agustin.camino@gmail.com>
2016-01-05 14:02:56 -0500

    Here be dragons

---
+++ LeadAlerts.Web.Tests/Controllers/HomeControllerTest.cs
@@ -0,0 +1,18 @@
+﻿using LeadAlerts.Web.Controllers;
+using NUnit.Framework;
+using TestStack.FluentMVCTesting;
+
+namespace LeadAlerts.Web.Tests.Controllers
+{
+    public class HomeControllerTest
+    {
+        [Test]
+        public void GivenACreateAction_ThenRendersDefaultView()
+        {
+            var controller = new HomeController();
+
+            controller.WithCallTo(c => c.Index())
+                .ShouldRenderDefaultView();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+# Instant Lead Alerts for C# and ASP.NET MVC
+
+This demo application shows how to implement instant lead alerts using C# and ASP.NET MVC. Notify sales reps or agents right away when a new lead comes in for a real estate listing or other high value channel.
+
+
+[![Build status](https://ci.appveyor.com/api/projects/status/7b6v4xetbn0uy6yc/branch/master?svg=true)](https://ci.appveyor.com/project/TwilioDevEd/lead-alerts-csharp/branch/master)
