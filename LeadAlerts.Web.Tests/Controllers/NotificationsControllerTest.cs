f3f023df1af69aa9c5255ea3b9280812dc024533
Agustin Camino <agustin.camino@gmail.com>
2016-01-06 09:46:30 -0500

    Fix failing tests

--- LeadAlerts.Web.Tests/Controllers/NotificationsControllerTest.cs
+++ LeadAlerts.Web.Tests/Controllers/NotificationsControllerTest.cs
@@ -1,9 +1,11 @@
-﻿using LeadAlerts.Web.Controllers;
+﻿using System.Web;
+using LeadAlerts.Web.Controllers;
 using LeadAlerts.Web.Domain;
 using LeadAlerts.Web.ViewModels;
 using Moq;
 using NUnit.Framework;
 using TestStack.FluentMVCTesting;
+using Twilio;
 
 namespace LeadAlerts.Web.Tests.Controllers
 {
@@ -12,7 +14,12 @@ namespace LeadAlerts.Web.Tests.Controllers
         [Test]
         public void GivenACreateAction_ThenItSendsAMessage()
         {
+            HttpContext.Current = new HttpContext(
+                new HttpRequest(null, "http://tempuri.org", null),
+                new HttpResponse(null));
+
             var mockMessageSender = new Mock<IMessageSender>();
+            mockMessageSender.Setup(ms => ms.Send(It.IsAny<string>())).Returns(new Message());
             var controller = new NotificationsController(mockMessageSender.Object);
 
             controller.WithCallTo(c => c.Create(new Lead()))
