7ae0d70a9c083dda2b04749066c07ab9aa7d592d
joliveros <jose.oliveros.1983@gmail.com>
2017-01-05 13:39:20 -0800

    Update tests

--- LeadAlerts.Web.Tests/Controllers/NotificationsControllerTest.cs
+++ LeadAlerts.Web.Tests/Controllers/NotificationsControllerTest.cs
@@ -1,11 +1,12 @@
 ﻿using System.Web;
 using LeadAlerts.Web.Controllers;
-using LeadAlerts.Web.Domain;
 using LeadAlerts.Web.ViewModels;
 using Moq;
 using NUnit.Framework;
 using TestStack.FluentMVCTesting;
 using Twilio;
+using Twilio.Clients;
+using Twilio.Http;
 
 namespace LeadAlerts.Web.Tests.Controllers
 {
@@ -18,14 +19,21 @@ namespace LeadAlerts.Web.Tests.Controllers
                 new HttpRequest(null, "http://tempuri.org", null),
                 new HttpResponse(null));
 
-            var mockMessageSender = new Mock<IMessageSender>();
-            mockMessageSender.Setup(ms => ms.Send(It.IsAny<string>())).Returns(new Message());
-            var controller = new NotificationsController(mockMessageSender.Object);
+            var twilioClientMock = new Mock<ITwilioRestClient>();
+            twilioClientMock.Setup(c => c.AccountSid).Returns("AccountSID");
+            twilioClientMock.Setup(c => c.RequestAsync(It.IsAny<Request>()))
+                            .ReturnsAsync(new Response(
+                                         System.Net.HttpStatusCode.Created,
+                                         "{\"account_sid\": \"ACaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\",\"api_version\": \"2010-04-01\",\"body\": \"O Slash: \\u00d8, PoP: \\ud83d\\udca9\",\"date_created\": \"Thu, 30 Jul 2015 20:12:31 +0000\",\"date_sent\": \"Thu, 30 Jul 2015 20:12:33 +0000\",\"date_updated\": \"Thu, 30 Jul 2015 20:12:33 +0000\",\"direction\": \"outbound-api\",\"error_code\": null,\"error_message\": null,\"from\": \"+14155552345\",\"messaging_service_sid\": \"MGaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\",\"num_media\": \"0\",\"num_segments\": \"1\",\"price\": \"-0.00750\",\"price_unit\": \"USD\",\"sid\": \"SMaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\",\"status\": \"sent\",\"subresource_uris\": {\"media\": \"/2010-04-01/Accounts/ACaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa/Messages/SMaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa/Media.json\"},\"to\": \"+14155552345\",\"uri\": \"/2010-04-01/Accounts/ACaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa/Messages/SMaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa.json\"}"
+                                     ));
 
-            controller.WithCallTo(c => c.Create(new Lead()))
-                .ShouldRedirectTo<HomeController>(home => home.Index());
+            var controller = new NotificationsController();
+            TwilioClient.SetRestClient(twilioClientMock.Object);
 
-            mockMessageSender.Verify(c => c.Send(It.IsAny<string>()), Times.Once);
+            controller.WithCallTo(c => c.Create(new Lead()));
+
+            twilioClientMock.Verify(
+                c => c.RequestAsync(It.IsAny<Request>()), Times.Once);
         }
     }
 }
--- LeadAlerts.Web.Tests/Domain/MessageSenderTest.cs
+++ LeadAlerts.Web.Tests/Domain/MessageSenderTest.cs
@@ -1,7 +1,10 @@
 ﻿using LeadAlerts.Web.Domain;
-using Moq;
 using NUnit.Framework;
 using Twilio;
+using Twilio.Http;
+using Twilio.Clients;
+using Moq;
+using System.Threading.Tasks;
 
 namespace LeadAlerts.Web.Tests.Domain
 {
@@ -10,13 +13,24 @@ namespace LeadAlerts.Web.Tests.Domain
         [Test]
         public void MessageSender_SendsAMessage()
         {
-            var mockClient = new Mock<TwilioRestClient>(null, null);
+            var twilioClientMock = new Mock<ITwilioRestClient>();
+            twilioClientMock.Setup(c => c.AccountSid).Returns("AccountSID");
+            twilioClientMock.Setup(c => c.RequestAsync(It.IsAny<Request>()))
+                              .ReturnsAsync(new Response(
+                                         System.Net.HttpStatusCode.Created,
+                                         "{\"account_sid\": \"ACaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\",\"api_version\": \"2010-04-01\",\"body\": \"O Slash: \\u00d8, PoP: \\ud83d\\udca9\",\"date_created\": \"Thu, 30 Jul 2015 20:12:31 +0000\",\"date_sent\": \"Thu, 30 Jul 2015 20:12:33 +0000\",\"date_updated\": \"Thu, 30 Jul 2015 20:12:33 +0000\",\"direction\": \"outbound-api\",\"error_code\": null,\"error_message\": null,\"from\": \"+14155552345\",\"messaging_service_sid\": \"MGaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\",\"num_media\": \"0\",\"num_segments\": \"1\",\"price\": \"-0.00750\",\"price_unit\": \"USD\",\"sid\": \"SMaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\",\"status\": \"sent\",\"subresource_uris\": {\"media\": \"/2010-04-01/Accounts/ACaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa/Messages/SMaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa/Media.json\"},\"to\": \"+14155552345\",\"uri\": \"/2010-04-01/Accounts/ACaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa/Messages/SMaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa.json\"}"
+                                     ));
 
-            var messageSender = new MessageSender(mockClient.Object);
-            messageSender.Send("message");
+            var messageSender = new MessageSender();
 
-            mockClient.Verify(c => c.SendMessage(
-                It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>()), Times.Once);
+            TwilioClient.SetRestClient(twilioClientMock.Object);
+
+            Task.Run(async () =>
+            {
+                await messageSender.SendAsync("message");
+                twilioClientMock.Verify(
+                c => c.RequestAsync(It.IsAny<Request>()), Times.Once);
+            }).GetAwaiter().GetResult();
         }
     }
 }
