f2732a5da732883c3ddedb62e0d31fc4119debde
Agustin Camino <agustin.camino@gmail.com>
2017-01-17 14:42:46 -0500

    Refactor `Configuration.cs` to use expression body

--- LeadAlerts.Web/Domain/Configuration.cs
+++ LeadAlerts.Web/Domain/Configuration.cs
@@ -4,28 +4,16 @@ namespace LeadAlerts.Web.Domain
 {
     public class Credentials
     {
-        public static string AccountSID
-        {
-            get { return WebConfigurationManager.AppSettings["TwilioAccountSid"]; }
-        }
+        public static string AccountSID => WebConfigurationManager.AppSettings["TwilioAccountSid"];
 
-        public static string AuthToken
-        {
-            get { return WebConfigurationManager.AppSettings["TwilioAuthToken"]; }
-        }
+        public static string AuthToken => WebConfigurationManager.AppSettings["TwilioAuthToken"];
     }
 
 
     public class PhoneNumbers
     {
-        public static string Twilio
-        {
-            get { return WebConfigurationManager.AppSettings["TwilioPhoneNumber"]; }
-        }
+        public static string Twilio => WebConfigurationManager.AppSettings["TwilioPhoneNumber"];
 
-        public static string Agent
-        {
-            get { return WebConfigurationManager.AppSettings["AgentPhoneNumber"]; }
-        }
+        public static string Agent => WebConfigurationManager.AppSettings["AgentPhoneNumber"];
     }
 }
